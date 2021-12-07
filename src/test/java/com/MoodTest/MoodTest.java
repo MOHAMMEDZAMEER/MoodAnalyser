93b645fd3639321f91e7b48ca3ec6702c8192493
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-12-07 03:17:26 -0400

    Should Return Happy

--- src/test/java/com/MoodTest/MoodTest.java
+++ src/test/java/com/MoodTest/MoodTest.java
@@ -5,15 +5,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import Com.MoodAnlyser.ModdAnalyser;
 
-/*TC1.1:-Should return Sad "iam in Sad Modd"
+/*TC1.1:-Should return Sad "I am in Sad MOOd"
  * */
 
 public class MoodTest  {
 
 	@Test
-	public void moodsad() {
+	public void moodSad() {
 		ModdAnalyser M = new ModdAnalyser();
 		assertEquals("SAD", M.moodAnalyser("Iam in Sad MOOd"));
 	}
-	
+	/*
+	 * Tc1.2 should Return Happy "I am in Any MOOD" 
+	 * */
+	@Test
+	public void moodHappy() {
+		ModdAnalyser M = new ModdAnalyser();
+		assertEquals("HAPPY", M.moodAnalyser("Iam in Any MOOD"));
+	}
 }
